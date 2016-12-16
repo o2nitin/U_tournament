e3b79b880e0272a0fa52f3631b5f5049b76dac39
Nitin Kumar Singh <nitinksingh11@gmail.com>
2016-12-16 23:26:31 +0530

    view for player standing created

--- tournament.sql
+++ tournament.sql
@@ -17,3 +17,17 @@ CREATE TABLE matches (
         winner integer REFERENCES players(p_id) NOT NULL,
         loser integer REFERENCES players(p_id) NOT NULL
 );
+
+CREATE VIEW standings AS
+SELECT players.p_id, players.name,
+(SELECT count(matches.winner)
+    FROM matches
+    WHERE players.p_id = matches.winner)
+    AS total_wins,
+(SELECT count(matches.m_id)
+    FROM matches
+    WHERE players.p_id = matches.winner
+    OR players.p_id = matches.loser)
+    AS total_matches
+FROM players
+ORDER BY total_wins DESC, total_matches DESC;
